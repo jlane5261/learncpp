d25983ce0c6702f1f9a6c1455bc64bdcffb4efa4
Josh <jlane5261@gmail.com>
2024-01-06 19:36:03 -0600

    added compatibility check files, a default version finder, and updated the build file to include g++ options

--- 0.7/build.sh
+++ 0.7/build.sh
@@ -1,5 +1,18 @@
 # debug
 #g++ -ggdb -Wall -o HelloWorld main.cpp
 
+
 # release
-g++ -O2 -DNDEBUG -Wall -o HelloWorld main.cpp
+g++ -O2 -DNDEBUG -std=c++17 -o HelloWorld main.cpp
+
+
+# disable compiler extensions
+#g++ -pedantic-errors -O2 -DNDEBUG -o HelloWorld main.cpp
+
+
+# Warning flags
+# -Wall, -Weffc++, -Wextra, -Wconversion, -Wsign-conversion, -Werror
+
+
+# Standards
+# -std=c++11, -std=c++14, -std=c++17, -std=c++20
